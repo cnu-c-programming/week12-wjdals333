ea5cf289654a163fd780e92045bf8145ed599c84
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-05-27 01:23:48 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,18 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    if (fp_src == NULL) return 0;
+
+    int c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        fputc(c, fp_dst);
+    }
+
+
+    fclose(fp_src);
+    fclose(fp_dst);
+    
 }
-
