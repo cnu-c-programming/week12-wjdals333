bea5541767e2e1d1b58f0e5a36edea4b676d0cbf
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-05-27 01:12:19 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,7 +6,20 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    if (fp == NULL) return 0;
 
+    int counts[26] = {0};
+    int c;
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            counts[c - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", i + 'a', counts[i]);
+    }
 
     fclose(fp);
 }
