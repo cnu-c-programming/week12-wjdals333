da4689f813065f7e600fe58b3fe819f1c0f83da2
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-05-27 10:37:22 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -8,11 +8,28 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    int num;
+    int temp;
 
+    while ((temp = fscanf(fp, "%d", &num)) != EOF) {
+        if (temp == 1) {
+            sum += num;
+        }
+        else {
+            char invalid_input[64];
+            fscanf(fp, "%s", invalid_input);
+
+            fprintf(stderr, "invalid input %s\n", invalid_input);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
     return 0;
 }
+
+
+
+
+
